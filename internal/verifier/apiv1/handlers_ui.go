fef06d0c64056098b47f71655800fa8e8e042f50
Joel Rangsmo <joel@siros.org>
2026-03-09 10:06:15 +0100

    Cherrypick changes from upstream PR #286

--- internal/verifier/apiv1/handlers_ui.go
+++ internal/verifier/apiv1/handlers_ui.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 	"time"
 	"vc/pkg/cache"
-	"vc/pkg/model"
 	"vc/pkg/openid4vp"
 
 	"github.com/google/uuid"
@@ -31,7 +30,7 @@ func (c *Client) UIMetadata(ctx context.Context) (*UIMetadataReply, error) {
 		SupportedWallets: c.cfg.Verifier.SupportedWallets,
 	}
 
-	for scope, constructor := range c.cfg.Common.CredentialConstructor {
+	for scope, constructor := range c.cfg.CredentialConstructor {
 		info := &UICredentialInfo{
 			Attributes: constructor.Attributes,
 		}
@@ -90,8 +89,7 @@ func (c *Client) UIInteraction(ctx context.Context, req *UIInteractionRequest) (
 		CodeChallengeMethod:      "",
 		Consent:                  false,
 		AuthenticSource:          "",
-		Identity:                 &model.Identity{},
-		Token:                    &cache.Token{},
+		// Identity and Token are nil until wallet presents credentials
 		Nonce:                    nonce,
 		EphemeralEncryptionKeyID: uuid.NewString(),
 		VerifierResponseCode:     "",
